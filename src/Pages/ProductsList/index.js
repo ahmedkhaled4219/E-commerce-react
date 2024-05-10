ee9b2c275895e80b4d7a9e9415b7df729fe3faa3
Ahmed Khaled <ahmedkhaled4219@gmail.com>
2024-05-10 17:49:59 +0300

    Lab1 Ajax

--- src/Pages/ProductsList/index.js
+++ src/Pages/ProductsList/index.js
@@ -1,25 +1,33 @@
-import { useEffect, useState } from "react";
-import { getProducts } from "../../apis/products";
+import { useState } from "react";
 import ProductCard from "../../components/Products/ProductCard/ProductCard";
 import Loader from "../../components/loader";
 
 export default function ProductsList() {
-  const [productsList, setProdcuts] = useState([]);
+  const [productsList, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    getProducts()
-      .then((res) => {
-        setProdcuts(res.data.products);
+  const loadProducts = () => {
+    setLoading(true);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://dummyjson.com/products", true);
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        const responseData = JSON.parse(xhr.responseText);
+        setProducts(responseData.products);
         setLoading(false);
-      })
-      .catch((error) => console.log(error));
-  }, []);
-  console.log(productsList);
+      } else {
+        setLoading(false);
+      }
+    };
+    xhr.send();
+  };
+
   return (
     <div className="container">
       <h2 className="text-center">Products list</h2>
       <hr />
+      <button onClick={loadProducts}>Load Products</button>{" "}
       {loading ? (
         <Loader />
       ) : (
